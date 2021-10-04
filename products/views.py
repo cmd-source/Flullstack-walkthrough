6feb2156bc8605f6eb04e4848df456560b9b4c19
cmd-source <conorcrosbie09@gmail.com>
2021-10-04 11:03:44 +0000

    products filter by catagories

--- products/views.py
+++ products/views.py
@@ -10,13 +10,13 @@ def all_products(request):
     products = Product.objects.all()
 
     query = None
-    category = None
+    categories = None
 
     if request.GET:
-        if category in request.GET:
+        if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__name__incategories)
-            categories = products.objects.filter(category__name__incategories)
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
 
         if 'q' in request.GET:
