7285614a72f65faf37074b2b68d5e035f8b4ca67
cmd-source <conorcrosbie09@gmail.com>
2021-10-05 08:15:45 +0000

    updated jQuare and imported Lower

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Product, Category
 from django.contrib import messages
+from django.db.models.functions import Lower
 from django.db.models import Q
 # Create your views here.
 
