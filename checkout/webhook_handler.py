1ea801330a64e7635b03e45b6610be551aa0ef36
cmd-source <conorcrosbie09@gmail.com>
2021-11-01 12:40:14 +0000

    saving an small changes made

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,11 +19,12 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
-        intent.data.object
-        print(intent)
+
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
